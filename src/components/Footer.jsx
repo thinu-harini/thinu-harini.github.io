b776815eb1363e923da23aa01450d3266586b202
Thinu Harini <inirahthedovahziii@gmail.com>
2024-02-17 22:46:28 +0530

    footer modify

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,22 @@
-import { useRef, useState } from "react";
-import { slideIn } from "../utils/motion";
+import React from "react";
 import { SectionWrapper } from "../hoc";
+import { SvgLogo } from "./logoSvg.jsx";
+import SocialIcons from './SocialIcons';
+import { styles } from '../styles';
 
 const Footer = () => {
   return (
-
-    <div className="footer">
-      <div className="footer-content">
+    <div className="footer rounded-2xl" style={{ backgroundColor: '#ffffff05' }}>
+      <div className={`flex flex-col items-center md:flex-row md:justify-between md:items-center sm:px-12 px-4 py-7`}>
+        <SvgLogo className={`mb-6 sm:mb-0`} />
+        <div className="text-center sm:text-right sm:mt-2">
+          <h1 className="hidden sm:block" style={{ fontStyle: 'italic' }}>
+            Let's Connect.
+          </h1>
+          <SocialIcons />
+        </div>
+      </div>
+      <div className="footer-content text-center mt-4">
         Designed and built by Thinu Harini <br />
         &#169; thinu-harini.github.io | All rights reserved
       </div>
@@ -14,5 +24,5 @@ const Footer = () => {
   )
 }
 
-export default Footer;
-// export default SectionWrapper(Footer, "footer");
+// export default Footer;
+export default SectionWrapper(Footer, "footer");
