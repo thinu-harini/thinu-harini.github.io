855d8824a1d6b6f864ac44bbc391551b2a96197e
Thinu Harini <inirahthedovahziii@gmail.com>
2024-02-18 00:48:12 +0530

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,15 +5,23 @@ import SocialIcons from './SocialIcons';
 
 const Footer = () => {
   return (
-    <div className="footer rounded-2xl" style={{ backgroundColor: '#ffffff05' }}>
-      <div className={`flex flex-col items-center md:flex-row md:justify-between md:items-center sm:px-12 px-4 py-7`}>
-        <SvgLogo className={`mb-6 sm:mb-0`} />
-        <div className="text-center sm:text-right sm:mt-2">
-          <h1 className="hidden sm:block" style={{ fontStyle: 'italic' }}>
-            Let's Connect.
-          </h1>
-          <SocialIcons />
-        </div>
+    // <div className="footer rounded-2xl" style={{ backgroundColor: '#ffffff05' }}>
+    //   <div className={`flex flex-col items-center md:flex-row md:justify-between md:items-center sm:px-12 px-4 py-7`}>
+    //     <SvgLogo className={`mb-6 sm:mb-0`} />
+    //     <div className="text-center sm:text-right sm:mt-2">
+    //       <h1 className="hidden sm:block" style={{ fontStyle: 'italic' }}>
+    //         Let's Connect.
+    //       </h1>
+    //       <SocialIcons />
+    //     </div>
+    //   </div>
+
+    <div className="footer">
+      <div className={`flex flex-col items-center md:items-center sm:px-4 px-4 py-4`}>
+        <h1 className="py-2">
+          Let's Connect.
+        </h1>
+        <SocialIcons />
       </div>
       <div className="footer-content text-center mt-4">
         Designed and built by Thinu Harini <br />
@@ -23,5 +31,5 @@ const Footer = () => {
   )
 }
 
-// export default Footer;
-export default SectionWrapper(Footer, "footer");
+export default Footer;
+// export default SectionWrapper(Footer, "footer");
