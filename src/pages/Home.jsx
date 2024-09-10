b1b58d08ca190a4f507a2406b7601561655acdd3
Thinu Harini <thinuharini.work@gmail.com>
2024-09-10 15:13:43 +0530

    Hexagon canvas removed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -67,9 +67,9 @@ const Home = ({ isDark }) => {
 
               </div>
 
-              <div className="hexagon-canvas">
+              {/* <div className="hexagon-canvas">
                 <HexagonCanvas />
-              </div>
+              </div> */}
 
             </div>
           </>
